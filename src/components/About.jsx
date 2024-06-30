e00f7f00d31aa9716cf6a5ccaa8858c1e60bb465
Benjamin Onoja <onojapeter90@gmail.com>
2024-06-30 15:51:35 +0100

    made style changes on about, hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,20 +3,20 @@ function About() {
     return (
         <div>
             <div className="px-8 mx-auto mb-[224px] max-w-screen-2xl md:px-12" id="About">
-                <h1 className="mb-24 text-3xl font-semibold text-center text-gray-300 uppercase font-poppins">About me</h1>
+                <h1 className="mb-20 text-3xl font-semibold text-center text-gray-300 uppercase font-poppins">About me</h1>
 
-                <div className="flex flex-col flex-wrap items-center justify-between gap-10 mx-auto md:flex-row">
-                    <div>
-                        <img className="w-80 rounded-xl" src="/avatar.jpg" alt="Peter" />
+                <div className="flex flex-col gap-y-10  justify-between  md:flex-row ">
+                    <div className="flex justify-center md:items-start w-full mx-auto max-w-[300px] md:w-[40%]">
+                        <img className="w-[300px] h-[300px] rounded-xl" loading="lazy" src="/avatar.jpg" alt="Peter" />
                     </div>
                     
-                    <div className="flex flex-col flex-wrap max-w-3xl gap-5  ">
+                    <div className="flex flex-col  lg:max-w-3xl gap-5  md:w-[55%]">
                         <h1 className="text-lg font-semibold tracking-wide text-left text-gray-200 font-lato">Introduction</h1>
                         <p className="text-lg text-gray-400 font-lato">My name is Onoja Peter, I'm a FullStack Developer based in Nigeria. I spend most of my time coding up things for the web, bringing designs to life and learning algorithms.</p>
 
                         <p className="text-lg text-gray-400 font-lato">My goal is to deliver, through code, unique and innovative solutions to complex problems. If my portfolio interests you, or you need an enthusiastic developer on your team, <a href="mailto:onojapeter90@gmail.com" className="text-indigo-500 hover:border-b-2 hover:border-indigo-500">I am available for hire</a></p>
 
-                        <div className="flex flex-col gap-4 md:flex-row md:gap-48">
+                        <div className="flex flex-col md:justify-between gap-4 md:flex-row md:gap-4">
                             <div className="mb-4">
                             <h2 className="mb-6 text-lg tracking-wide text-left text-gray-200 font-lato">Languages and Technologies</h2>
                             <ul className="space-y-2 text-gray-400 font-lato">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import { MdSend } from "react-icons/md";
 import Button from "./Button"
 
 
 function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        
+        try {
+            const res = await axios.post('http://localhost:5000/send-email', { name, email, message });
+            setTimeout(() => {
+                setResponse({ type: 'success', message: res.data });
+                setName('');
+                setEmail('');
+                setMessage('');
+            }, 1000);
+        } catch (error) {
+            setResponse({ type: 'error', message: 'Failed to send message. Please try again later.' });
+            console.log(response)
+        } finally{
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+            
+            setTimeout(() => {
+                setResponse(null);
+            }, 7000);
+        }
+    };
+
     return (
         <div className="px-8 mx-auto mb-[224px] max-w-screen-2xl md:px-12" id="Contact">
             <div className="flex flex-col items-center gap-6">
@@ -10,30 +44,49 @@ function Contact() {
                 <p className="max-w-xl mb-10 text-lg text-center text-gray-400 font-lato">You might want to develop a website, application or discuss anything related. Feel free to leave a message below</p>
             </div>
             
-            <form className="flex flex-col items-center mx-auto space-y-4">
+            {response && (
+                <div className={`fixed top-0 left-0 w-full ${response.type === 'success' ? 'bg-green-500' : 'bg-red-500'} text-white py-3 text-center`}>
+                    <p>{response.message}</p>
+                </div>)
+            }
+
+            <form  onSubmit={handleSubmit} className="flex flex-col items-center mx-auto space-y-4">
                 <div className="flex flex-col w-full max-w-lg gap-2 mb-2">
                     <label className="text-sm text-gray-400" htmlFor="name">Name</label>
-                    <input type="text" id="name" placeholder="Name" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600" />
+                    <input value={name} onChange={(e) => setName(e.target.value)} required  type="text" id="name" name='name' placeholder="Name" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600" />
                 </div>
                 
                 <div className="flex flex-col w-full max-w-lg gap-2 mb-2">
-                <label className="text-sm text-gray-400" htmlFor="name">Email</label>
-                <input type="email" id="name" placeholder="Email" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600" />
+                <label className="text-sm text-gray-400" htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} required  type="email" id="email" name='email' placeholder="Email" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600" />
                 </div>
                 
                 <div className="flex flex-col w-full max-w-lg gap-2 mb-2">
                 <label className="text-sm text-gray-400" htmlFor="message">Message</label>
-                <textarea rows={4} type="text" name="message" id="message" placeholder="Message" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600"></textarea>
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)} required  rows={4} type="text" name="message" id="message" placeholder="Message" className="px-3 py-3 text-gray-300 bg-transparent border border-gray-700 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 placeholder:text-sm placeholder:text-gray-600"></textarea>
                </div> 
+
+               <div className="pt-8 text-center">
+                    <Button>
+                        <div className="flex items-center gap-1">
+                            
+                                {!loading ? 
+                                    <div className='flex gap-2'><p className="text-sm font-lato text-[16px]">Send</p> <MdSend className="w-5 h-5" /></div>
+                                     : 
+                                    (<div className='flex gap-2 cursor-not-allowed'>
+                                        <p className="text-sm font-lato text-[16px]">Sending...</p>
+                                        <svg class="animate-spin h-5 w-5 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.004 8.004 0 014.01 4.01h-.011M20 12c0-4.418-3.582-8-8-8v4c2.764 0 5.1 1.432 6.491 3.603M4.01 19.99A8.004 8.004 0 0112.001 20v4c4.418 0 8-3.582 8-8h-4z"></path>
+                                        </svg>
+                                    </div>) 
+                                }
+                            
+                        </div>
+                    </Button>
+                </div>
             </form>
-            <div className="mt-8 text-center">
-                <Button>
-                    <div className="flex items-center gap-1">
-                        <p className="text-sm font-lato text-[16px]">Send</p>
-                        <MdSend className="w-5 h-5" />
-                    </div>
-                </Button>
-            </div>  
+              
         </div>
     )
 }
